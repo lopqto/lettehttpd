36b5d19fe9bc3b71f331cb45dc3b29ce1fe81465
lopqto <morpix@protonmail.com>
2019-02-28 21:45:54 +0330

    added parser options (for beta release)

--- src/lettehttpd.cr
+++ src/lettehttpd.cr
@@ -1,6 +1,12 @@
-# TODO: Write documentation for `Lettehttpd`
-module Lettehttpd
-  VERSION = "0.1.0"
+require "option_parser"
 
-  # TODO: Put your code here
+# Defaults varibles
+content_directory = "./"
+listen_port = 8080
+listen_addr = "0.0.0.0"
+
+OptionParser.parse! do |parser|
+    parser.on("-h", "--help", "show this help message and exit") { puts parser; exit }
+    parser.on("-d DIR", "--directory DIR", "specify a directory") { |dir| content_directory = dir }
+    parser.on("-l ip:port", "--listen ip:port", "specify listen address and port") { |listen_vars| listen_addr, listen_port = listen_vars.split(':') }
 end
---
+++ src/lettehttpd/version.cr
@@ -0,0 +1,4 @@
+module Lettehttpd
+    VERSION = "0.1.0"
+end
+  
